cface8f63f70d3015584d56fee13465b0f3ac288
sakethkumar1 <134069726+Sakethuchiha@users.noreply.github.com>
2024-06-17 09:14:36 +0000

    variables and datatypes

---
+++ 01_basics/variable.js
@@ -0,0 +1,8 @@
+const accountId = "1234443"
+var accountState = "telangana"
+let accountCity="hyderabad"
+let accountPassword;
+// changing the value of const variables are not allowed 
+// using let is recommended than using the var becuase of issue in blockspace and functional space 
+console.table([accountId,accountCity,accountState,accountPassword])
+// using variables with let keyword is recommended instead of directly declaring them .
