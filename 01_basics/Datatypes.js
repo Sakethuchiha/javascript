5b952588e4709a965a34fe8438b8fb7ac794ea3d
sakethkumar1 <134069726+Sakethuchiha@users.noreply.github.com>
2024-06-26 05:39:59 +0000

    conversions

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,9 @@
+let age = 20
+console.log("Saketh")
+let name = "Sakethk"
+let accountState;
+//null is a standole type and null is an object 
+let city = null
+// we get undefined if we dont define anything 
+console.log(typeof null)
+//datatypes in js are number,string,null,object,symbol=unique,bigint,boolean etc
---
+++ 01_basics/conversions.js
@@ -0,0 +1,39 @@
+let score = 33
+console.log(typeof score)
+let ChangedScore = String (score)
+console.log(typeof ChangedScore)
+console.log(ChangedScore)
+
+
+// if string is "33abc" then the number converted would be NaN
+// undefined = Nan , null = 0 
+
+let isLoggedin = 1
+console.log(typeof isLoggedin)
+let BooleanChangedlogg = Boolean (isLoggedin)
+console.log(typeof BooleanChangedlogg)
+console.log(BooleanChangedlogg)
+
+// if converted to boolean then 
+// ""=> false , "saketh" is true 
+//operations***************************
+let str1 = "hello"
+let str2 = " saketh"
+let str3 = str1+str2
+console.log(str3)
+console.log(1+1)
+console.log(2-2)
+//same for all arithmetic operations
+console.log(1+"2")
+//12
+console.log("2"+1)
+//21
+console.log(1+2+"2")
+//32
+console.log("2"+1+1)
+//211
+
+console.log(+true)
+//1
+console.log(+"")
+//0
