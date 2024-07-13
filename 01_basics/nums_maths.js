08a1a4c9733d9959c7283c78c443ad72725f10a5
sakethkumar1 <134069726+Sakethuchiha@users.noreply.github.com>
2024-07-13 18:33:18 +0000

    maths and numbers

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,20 @@
+
+
+const A = new Number(2000000)
+// console.log(A)
+//we can explicitly store as number using new 
+
+// console.log(A.toLocaleString('en-IN'))
+const B =1233.990
+// console.log(B.toPrecision(4))
+///use precision wisely
+////////maths//////
+
+console.log(Math)
+console.log(Math.PI)
+console.log(Math.round(52.552))
+//ceil and round are also used 
+const min =1
+const max =6
+
+console.log(Math.floor(Math.random()*(max-min)+1)+min)
